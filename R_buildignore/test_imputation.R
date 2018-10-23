449cdebe006b999ddc7edd3a52ccb96eb4fd42d4
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2018-10-23 14:41:26 +0800

    Names of functions

--- R_buildignore/test_imputation.R
+++ R_buildignore/test_imputation.R
@@ -7,6 +7,20 @@ library(imputeFin)
 
 # impute
 
+imputePrices <- function(Y ...) {
+  imputeFin(Y, ar1 = 1 ...)
+  
+}
+
+imputeVolumes
+
+imputeFin <- function(Y, phi = NA, ar1 = NA, dist = "gaussian") {
+  Y <- as.vector(Y)
+  
+}
+
+
+
 # performance measure
 
 # plots
