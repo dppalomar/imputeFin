85cb3a2b85f45edcf94cc1fbafd6c0285c4463d3
Daniel Palomar <palomar@Daniels-MacBook-Air.local>
2019-02-27 16:39:50 +0800

    small things

--- R_buildignore/test_AR1_gaussian.R
+++ R_buildignore/test_AR1_gaussian.R
@@ -1,31 +1,32 @@
 library(imputeFin)
 library(xts)
 
+# generate the complete time series
 phi0 <- 0
 phi1 <- 1
 sigma2 <- 0.01 
-n <- 200
-n_miss <- 20 
+n <- 300
+n_miss <- 30 
 n_drop <- 100
 n_total <- n + n_drop
 data <- vector(length = n_total)
 epsilon <- vector(length = n_total)  # innovations
 data[1] <- 0
 for (i in 2:n_total) {
-  epsilon[i-1] <- rnorm(1, 0, sqrt(sigma2)) 
-  data[i] <- phi0 + phi1 * data[i-1] + epsilon[i]
+  epsilon[i] <- rnorm(1, 0, sqrt(sigma2)) 
+  data[i] <- phi0 + phi1 * data[i - 1] + epsilon[i]
 }
 data <- data[(n_drop + 1):n_total]  # drop the first n_drop to reduce the influence of initial point
 y_orig <- xts(data,  seq(as.Date("2016-01-01"), length = n, by = "days"))
 
 # creat missing values
-index_miss <- sample(2:(n - 1), n_miss)
+index_miss <- sort(sample(2:(n - 1), n_miss))
 y <- y_orig
 y[index_miss] <- NA
 
-# estimation 
-estimation_result <- estimateAR1Gaussian(y, random_walk = FALSE, zero_mean = FALSE, stop_crt = 1e-10,  
-                                         max_iteration = 1000, output_iterates = TRUE)
+# test the estimation function
+estimation_result <- estimateAR1Gaussian(y, random_walk = FALSE, zero_mean = FALSE, ftol = 1e-10,  
+                                         maxiter = 1000, output_iterates = TRUE)
 layout(matrix(c(1, 2, 3, 4)), heights = c(1,1,1,1))
 par(mar = c(4, 5, 0.1, 0.5))
 n_iter <- length(estimation_result$phi0_iterate)
@@ -35,15 +36,14 @@ plot(1:n_iter,estimation_result$sigma2_iterate, pch=19, cex.lab = 1.5, cex.axis 
 plot(1:n_iter,estimation_result$f_iterate, pch=19, cex.lab = 1.5, cex.axis = 1.2,  type='o',col = "green",  xlab ="Iteration k", ylab = "obj" )
 
 
-# imputation
-param = list("phi0" = 1,
-             "phi1" = 0.5,
-             "sigma2" = 0.01)
-imputation_result <- imputeAR1Gaussian(y, n_sample = 1, param = NULL, random_walk = FALSE, zero_mean = FALSE)
+# test the imputation function
+param = list("phi0" = phi0,
+             "phi1" = phi1,
+             "sigma2" = sigma2)
+imputation_result <- imputeAR1Gaussian(y, n_sample = 1, param = NULL, random_walk = FALSE, zero_mean = TRUE)
 y_imputed <- imputation_result$y_imputed
-#imputation_result = imputeAR1Gaussian(y, n_sample = 2, param)
 layout(1, heights = 1)
 par(mar=c(4, 5, 4, 5))
 plot(1:n, y_imputed, type = 'o', pch = 19,  ylab = 'y')
 points(index_miss, y_imputed[index_miss, 1], col = 'red', pch = 19)
-legend( x="topright", col = c("black","red"), pch = c(19, 19), legend = c("observed values", "imputed values") )
+legend( x="topright", col = c("black", "red"), pch = c(19, 19), legend = c("observed values", "imputed values"))
