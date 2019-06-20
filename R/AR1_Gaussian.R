e6271138ac59a07b36b854b8ffe0d340839d7c77
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2019-06-20 13:39:44 +0800

    Help on data.

---
+++ R/AR1_Gaussian.R
@@ -0,0 +1,13 @@
+#' Random sets of stock market data from the S&P 500, over random periods of two years with a random selection of 50 stocks of each universe.
+#'
+#' @docType data
+#'
+#' @usage data(dataset10)
+#'
+#' @format A list of \code{xts} objects with the prices of random sets of stock market data from the S&P 500 over random periods of two years with a random selection of 50 stocks of each universe.
+#'
+#' @source \href{https://finance.yahoo.com}{Yahoo! Finance}
+#'
+#' @keywords datasets
+#'
+"dataset10"
