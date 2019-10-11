28a3fe2ed651ffacdc4b17a1d03ff81508ee4bdb
Junyanliu1993 <38105215+Junyanliu1993@users.noreply.github.com>
2019-10-11 15:17:11 +0800

    vignette

--- R_buildignore/test_AR1_gaussian.R
+++ R_buildignore/test_AR1_gaussian.R
@@ -5,13 +5,13 @@ data(ts_AR1_Gaussian)
 # test the estimation function
 y_missing <- ts_AR1_Gaussian$y_missing
 estimation_result <- estimateAR1Gaussian(y_missing, random_walk = FALSE, zero_mean = FALSE,
-                                         return_iterates = FALSE, return_condMeanCov = FALSE,
+                                         return_iterates = TRUE, return_condMeanCov = TRUE,
                                          tol = 1e-10,  maxiter = 1000)
 
 
 # test the imputation function
-imputation_result <- imputeAR1Gaussian(y_missing, n_samples = 3, random_walk = FALSE, zero_mean = FALSE,
-                                       return_estimates = FALSE)
+imputation_result <- imputeAR1Gaussian(y_missing[ , 1], n_samples = 3, random_walk = FALSE, zero_mean = FALSE,
+                                       return_estimates = TRUE)
 y_imputed <- imputation_result$y_imputed.1
 plotImputed(y_imputed, column = 1, type = "simple")
 
