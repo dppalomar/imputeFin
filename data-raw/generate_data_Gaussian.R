f2d1391dde1ab81ef84331cd9d4ee64c1dcf0a2a
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2019-12-04 22:45:51 +0800

    Third column of Gaussian data regenerated.

--- data-raw/generate_data_Gaussian.R
+++ data-raw/generate_data_Gaussian.R
@@ -23,19 +23,23 @@ colnames(y_orig) <- c("a", "b", "c")
 y_missing_numeric <- y_orig
 index_miss1 <- round(n/2) + 1:n_miss
 index_miss2 <- sort(sample(2:(n - 1), n_miss))
-# index_miss3 <- c(1,sort(sample(2:(n - 1), n_miss - 2)), n)
-# index_miss3 <- c(1,2,3,10)
+index_miss3 <- union(index_miss1, index_miss2)
 y_missing_numeric[index_miss1, 1] <- NA
 y_missing_numeric[index_miss2, 2] <- NA
+y_missing_numeric[index_miss3, 3] <- NA
 # y_missing_numeric[index_miss3, 3] <- NA
 y_missing <- zoo(y_missing_numeric, seq(as.Date("2016-01-01"), length = n, by = "days"))
 ts_AR1_Gaussian <- list("y_missing" = y_missing,
                         "phi0"      = phi0,
                         "phi1"      = phi1,
                         "sigma2"    = sigma2)
+#index_miss1 <- which(is.na(ts_AR1_Gaussian$y_missing[, 1]))
+#index_miss2 <- which(is.na(ts_AR1_Gaussian$y_missing[, 2]))
+#index_miss3 <- union(index_miss1, index_miss2)
+#ts_AR1_Gaussian$y_missing[index_miss3, 3] <- NA
+
 #
 # this is to save the data to the package
 #
-#save(y_missing, file = "data-raw/y_missing.RData")
 usethis::use_data_raw()
 usethis::use_data(ts_AR1_Gaussian, overwrite = TRUE)
