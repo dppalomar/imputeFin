74a33541c5314d56913c83e01b5fa1589f0d3981
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2020-07-06 23:08:23 +0800

    Data generation revised.

--- data-raw/generate-ts_AR1_Gaussian.R
+++ data-raw/generate-ts_AR1_Gaussian.R
@@ -1,4 +1,4 @@
-# generate the data 
+# generate complete data
 phi0 <- 1
 phi1 <- 0.5
 sigma2 <- 0.01 
@@ -15,29 +15,28 @@ for (i in 2:n_total) {
 }
 data <- data[(n_drop + 1):n_total]  # drop the first n_drop to reduce the influence of initial point
 
+# repeat the time series m times
 m <- 3
-y_orig<- matrix(rep(data, m), nrow = n, ncol = m)
+y_orig <- matrix(rep(data, m), nrow = n, ncol = m)
 colnames(y_orig) <- c("a", "b", "c")
 
-# creat missing values
-y_missing_numeric <- y_orig
+# create missing values
 index_miss1 <- round(n/2) + 1:n_miss
 index_miss2 <- sort(sample(2:(n - 1), n_miss))
 index_miss3 <- union(index_miss1, index_miss2)
+#index_miss3 <- c(1,sort(sample(2:(n - 1), n_miss - 2)), n)
+y_missing_numeric <- y_orig
 y_missing_numeric[index_miss1, 1] <- NA
 y_missing_numeric[index_miss2, 2] <- NA
 y_missing_numeric[index_miss3, 3] <- NA
-y_missing <- zoo(y_missing_numeric, seq(as.Date("2016-01-01"), length = n, by = "days"))
+y_missing <- zoo::zoo(y_missing_numeric, seq(as.Date("2016-01-01"), length = n, by = "days"))
+
+
+# create variable
 ts_AR1_Gaussian <- list("y_missing" = y_missing,
                         "phi0"      = phi0,
                         "phi1"      = phi1,
                         "sigma2"    = sigma2)
-#index_miss1 <- which(is.na(ts_AR1_Gaussian$y_missing[, 1]))
-#index_miss2 <- which(is.na(ts_AR1_Gaussian$y_missing[, 2]))
-#index_miss3 <- union(index_miss1, index_miss2)
-#ts_AR1_Gaussian$y_missing[index_miss3, 3] <- NA
-
-
 
 # save the data to the package
 save(ts_AR1_Gaussian, file = "data/ts_AR1_Gaussian.RData", version = 2)
--- data-raw/generate-ts_AR1_t.R
+++ data-raw/generate-ts_AR1_t.R
@@ -1,3 +1,4 @@
+# generate complete data
 phi0 <- 0
 phi1 <- 1 
 sigma2 <- 0.01 
@@ -7,7 +8,7 @@ n_miss <- 0.1 * n
 n_drop <- 100
 n_total <- n + n_drop
 data <- vector(length = n_total)
-epsilon <- vector(length = n_total - 1)# innovations
+epsilon <- vector(length = n_total - 1)  # innovations
 data[1] <- 0
 for (i in 2:n_total) {
   epsilon[i] <- rt(1, nu) * sqrt(sigma2)
@@ -15,33 +16,29 @@ for (i in 2:n_total) {
 }
 data <- data[(n_drop + 1):n_total]  # drop the first n_drop to reduce the influence of initial point
 
+# repeat the time series m times
 m <- 3
-y_orig<- matrix(rep(data, m), nrow = n, ncol = m)
+y_orig <- matrix(rep(data, m), nrow = n, ncol = m)
 colnames(y_orig) <- c("a", "b", "c")
 
-# creat missing values
-y_missing_numeric <- y_orig
+# create missing values
 index_miss1 <- round(n/2) + 1:n_miss
 index_miss2 <- sort(sample(2:(n - 1), n_miss))
 index_miss3 <- union(index_miss1, index_miss2)
 #index_miss3 <- c(1,sort(sample(2:(n - 1), n_miss - 2)), n)
-
+y_missing_numeric <- y_orig
 y_missing_numeric[index_miss1, 1] <- NA
 y_missing_numeric[index_miss2, 2] <- NA
-# y_missing_numeric[index_miss3, 3] <- NA
-# index_miss3 <- c(1,sort(sample(2:(n - 1), n_miss - 2)), n)
-y_missing <- zoo(y_missing_numeric, seq(as.Date("2016-01-01"), length = n, by = "days"))
+y_missing_numeric[index_miss3, 3] <- NA
+y_missing <- zoo::zoo(y_missing_numeric, seq(as.Date("2016-01-01"), length = n, by = "days"))
+
+
+# create variable
 ts_AR1_t = list("y_missing" = y_missing,
                 "phi0"      = phi0,
                 "phi1"      = phi1,
                 "sigma2"    = sigma2,
                 "nu"        = nu)
-#index_miss1 <- which(is.na(ts_AR1_t$y_missing[, 1]))
-#index_miss2 <- which(is.na(ts_AR1_t$y_missing[, 2]))
-#index_miss3 <- union(index_miss1, index_miss2)
-#ts_AR1_t$y_missing[index_miss3, 3] <- NA
-
-
 
 # save the data to the package
 save(ts_AR1_t, file = "data/ts_AR1_t.RData", version = 2)
